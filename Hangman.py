85a975e244e3284328f2343f3ec31380b4e0a146
Zachary Meyer <zach.w.meyer@gmail.com>
2019-05-14 10:32:54 -0700

    added temp_word var, replaces each letter guessed correctly with a blank char. when temp_word is blank you won the game.

--- Hangman.py
+++ Hangman.py
@@ -4,15 +4,18 @@ with open('word list.txt') as f:
     word_list = f.readlines()
 
 word = word_list[random.randint(0, 1000)]
-guess_word = ''
-print(word)
+temp_word = word
+# print(word)
 
-while guess_word != word:
+while len(temp_word) > 1:
     print('Guess a letter:')
     letter = input().lower()
-    if letter in word:
-        print(f'{letter} is in the word {word.count(letter)} times')
-    elif letter not in word:
+    if letter in temp_word:
+        print(f'{letter} is in the word {temp_word.count(letter)} times, you have '
+              f'{len(temp_word) - temp_word.count(letter) - 1} letters left.')
+        temp_word = temp_word.replace(letter, '')
+    elif letter not in temp_word:
         print(f'{letter} is not in the word..')
 
-# print(word)
+if len(temp_word) <= 1:
+    print(f'You won, the word was {word}')
